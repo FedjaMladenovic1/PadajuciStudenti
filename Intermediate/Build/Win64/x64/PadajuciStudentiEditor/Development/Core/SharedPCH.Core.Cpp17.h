ae59f40bf2f8982c0898c3833a9ed4477e892220
Fedja <fm20180328@student.fon.bg.ac.rs>
2024-06-04 11:46:40 +0200

    first commit

---
+++ Intermediate/Build/Win64/x64/PadajuciStudentiEditor/Development/Core/SharedPCH.Core.Cpp17.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "C:/Users/Fedja/Documents/Unreal Projects/PadajuciStudenti/Intermediate/Build/Win64/x64/PadajuciStudentiEditor/Development/Core/SharedDefinitions.Core.Cpp17.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ Intermediate/Build/Win64/x64/PadajuciStudentiEditor/Development/Engine/SharedPCH.Engine.Cpp20.InclOrderUnreal5_0.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "C:/Users/Fedja/Documents/Unreal Projects/PadajuciStudenti/Intermediate/Build/Win64/x64/PadajuciStudentiEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.InclOrderUnreal5_0.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
